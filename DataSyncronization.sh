73e24055910e9f63c272228503d03ed0050d4ee0
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2015-03-09 18:49:18 +0100

    Removed syncronization of configuration from DataSyncronization.sh script.

--- DataSyncronization.sh
+++ DataSyncronization.sh
@@ -114,7 +114,7 @@ for RUN in ${DATA_GLOBALPATHS[@]}; do
     PrintSituationDataFolder ${BETAVALUES[@]}
     printf "\n\e[0;32m--------------------------------------------------------------\e[0m\n"
     # Then syncronize
-    python ${!identity} --syncConfs -f=$BETASFILE
+    python ${!identity} -f=$BETASFILE
     # Then clean data files
     CleanDataFiles ${BETAVALUES[@]}
     # For Staggered runs unmerge pbp file and check if it was ok
