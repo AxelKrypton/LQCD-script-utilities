3e89352a63e937eaaf92e9ea9ff31bf7b1348b25
Christopher Czaban <czaban@th.physik.uni-frankfurt.de>
2015-09-14 11:38:40 +0200

    Modified script in order to be able to 1.create ntXX directories, 2.use the integration tuning, 3.the job info functionality.

--- IntegrationTuning/UserSpecificVariables_cczaban.sh
+++ IntegrationTuning/UserSpecificVariables_cczaban.sh
@@ -1,10 +1,9 @@
 # Paths on LOEWE using CL2QCD
 USER_MAIL="czaban@th.physik.uni-frankfurt.de"
-HMC_BUILD_PATH="clhmc/build/RefExec"
-SIMULATION_PATH="IntegratorTest"
-HOME_DIR="/home/hfftheo/czaban" 
-WORK_DIR="/scratch/hfftheo/czaban" 
-SCRIPT_DIR="$HOME_DIR/Script/tmLQCD_Juqueen"
+HMC_BUILD_PATH="cl2qcd/build/RefExec"
+SIMULATION_PATH="IntegratorTestWilson"
+HOME_DIR="/lustre/nyx/lcsc/cczaban" 
+WORK_DIR="/lustre/nyx/lcsc/cczaban" 
 PRODUCEJOBSCRIPTSH="$HOME_DIR/Script/JobScriptAutomation/ProduceJobScript.sh"
 PRODUCEINPUTFILESH="$HOME_DIR/Script/JobScriptAutomation/ProduceInputFile.sh"
 HMC_FILENAME="hmc_ref"
@@ -13,6 +12,7 @@ INPUTFILE_NAME="hmc.input"
 JOBSCRIPT_PREFIX="job.cl2qcd.Thermalize"
 OUTPUTFILE_NAME="hmc_output"
 THERMALIZED_CONFIGURATIONS_PATH="$HOME_DIR/$SIMULATION_PATH/Thermalized_Configurations"
-GPU_PER_NODE=4
+GPU_PER_NODE=1
 JOBSCRIPT_LOCALFOLDER="JobScripts"
 
+#SCRIPT_DIR="$HOME_DIR/Script/tmLQCD_Juqueen"
