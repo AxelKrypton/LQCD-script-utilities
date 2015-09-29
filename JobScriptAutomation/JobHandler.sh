c6150ceae10a54e0181b2db636e71aaa9d905f06
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2015-09-29 09:44:36 +0200

    Edited some output.

--- JobScriptAutomation/JobHandler.sh
+++ JobScriptAutomation/JobHandler.sh
@@ -257,7 +257,6 @@ elif [ $COMPLETE_BETAS_FILE = "TRUE" ]; then
 
     CompleteBetasFile
     
-    #echo $UNCOMMENT_BETAS
 elif [ $UNCOMMENT_BETAS = "TRUE" ] || [ $COMMENT_BETAS = "TRUE" ]; then
 
 	UncommentEntriesInBetasFile
