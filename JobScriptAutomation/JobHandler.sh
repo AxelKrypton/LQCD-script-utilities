85c35454825024082ac006718b810dc35dde5acb
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2016-01-12 13:24:17 +0100

    Fixed some bugs (removed srun for multiple runs in JobHandler for L-CSC).

--- JobScriptAutomation/JobHandler.sh
+++ JobScriptAutomation/JobHandler.sh
@@ -159,7 +159,7 @@ HOME_DIR_WITH_BETAFOLDERS="$HOME_DIR/$SIMULATION_PATH$PARAMETERS_PATH"
 WORK_DIR_WITH_BETAFOLDERS="$WORK_DIR/$SIMULATION_PATH$PARAMETERS_PATH"
 
 if [ "$HOME_DIR_WITH_BETAFOLDERS" != "$(pwd)" ]; then
-        printf "\n\e[0;31m HOME_DIR_WITH_BETAFOLDERS=$HOME_DIR_WITH_BETAFOLDERS\n"
+    printf "\n\e[0;31m HOME_DIR_WITH_BETAFOLDERS=$HOME_DIR_WITH_BETAFOLDERS\n"
 	printf "\e[0;31m Constructed path to directory containing beta folders does not match the actual position! Aborting...\n\n\e[0m"
 	exit -1
 fi
@@ -227,6 +227,7 @@ elif [ $THERMALIZE = "TRUE" ] || [ $CONTINUE_THERMALIZATION = "TRUE" ]; then
     elif [ $CONTINUE_THERMALIZATION = "TRUE" ]; then
         ProcessBetaValuesForContinue
     fi
+    
     SubmitJobsForValidBetaValues #TODO: Declare all possible local variable in this function as local!
     
 elif [ $CONTINUE = "TRUE" ]; then
