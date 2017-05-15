58feb6f90836c9a0d8e7eaa5cb7c57c22a12be51
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2017-05-15 09:01:02 +0200

    Added new user to cluster usage script

--- ClusterUsage.sh
+++ ClusterUsage.sh
@@ -57,7 +57,7 @@ function PrintClusterUsageInformation(){
 #--------------------------------------------------------------------------------#
 
 #Variables to be changed, hardcoded for the moment
-USERS_LIST=( "czaban" "cuteri" "sciarra" )
+USERS_LIST=( "czaban" "cuteri" "sciarra" "witham" )
 CLUSTER_NAME='LOEWE'
 PARTITION='gpu'
 REMOTE_NAME='uni'
@@ -67,7 +67,7 @@ PRINT_ONLY_TO_SHELL='FALSE'
 if [ "$(hostname)" = "lxlcsc0001" ]; then
     CLUSTER_NAME='LCSC'
     PARTITION='lcsc'
-    USERS_LIST=( "cczaban" "fcuteri" "asciarra" )
+    USERS_LIST=( "cczaban" "fcuteri" "asciarra" "owitham" )
 fi    
 OUTPUT_FILE="${CLUSTER_NAME}_usage"
 EXCLUDED_NODES_FILENAME="${CLUSTER_NAME}_excludedNodes"
