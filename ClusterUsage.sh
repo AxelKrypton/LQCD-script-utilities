fe25152eb92f91cc42e57cae96202bc7694a6426
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2018-10-16 15:38:17 +0200

    Update users for cluster usage script

--- ClusterUsage.sh
+++ ClusterUsage.sh
@@ -67,7 +67,7 @@ PRINT_ONLY_TO_SHELL='FALSE'
 if [ "$(hostname)" = "lxlcsc0001" ]; then
     CLUSTER_NAME='LCSC'
     PARTITION='lcsc'
-    USERS_LIST=( "cczaban" "fcuteri" "asciarra" "owitham" )
+    USERS_LIST=( "fcuteri" "asciarra" "aschoen" )
 fi    
 OUTPUT_FILE="${CLUSTER_NAME}_usage"
 EXCLUDED_NODES_FILENAME="${CLUSTER_NAME}_excludedNodes"
